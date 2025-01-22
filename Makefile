f32e09d1cb0365c96f634b1e56638475e0951fef
seojilee <seojilee@student.42seoul.kr>
2025-01-22 11:47:16 +0900

    add: a prototype of get_next_line and a brief test main

---
+++ Makefile
@@ -0,0 +1,43 @@
+.DEFAULT_GOAL = all
+
+NAME=get_next_line
+CC=cc
+CFLAGS=-Wall -Wextra -Werror -MMD -MP -MF $(DEPS_DIR)/$*.d
+SRCS= \
+	 main.c \
+	 get_next_line.c \
+
+RM=rm -fr
+
+OBJS_DIR=.objs
+OBJS=$(addprefix $(OBJS_DIR)/, $(SRCS:.c=.o))
+
+DEPS_DIR=.deps
+DEPS=$(addprefix $(DEPS_DIR)/, $(SRCS:.c=.d))
+-include $(DEPS)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(LINK.o) $(OUTPUT_OPTION) $(OBJS)
+
+$(OBJS_DIR)/%.o: %.c | $(OBJS_DIR) $(DEPS_DIR)
+	$(COMPILE.c) $(OUTPUT_OPTION) $<
+
+$(OBJS_DIR):
+	mkdir -p $(OBJS_DIR)
+
+$(DEPS_DIR):
+	mkdir -p $(DEPS_DIR)
+
+clean:
+	$(RM) $(OBJS) $(DEPS)
+	$(RM) $(OBJS_DIR) $(DEPS_DIR)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean
+	$(MAKE) all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seojilee <seojilee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 11:39:40 by seojilee          #+#    #+#             */
+/*   Updated: 2025/01/22 11:45:17 by seojilee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Please give a file to read\n");
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("open");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("next_line: %s\n", line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
