e88d009d6dd4ce1c59737cf99cef04b03affcc73
seojilee <seojilee@student.42seoul.kr>
2025-01-23 10:24:44 +0900

    change: remove str utilities and add a custon realloc

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,42 @@
 /*   By: seojilee <seojilee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 11:39:05 by seojilee          #+#    #+#             */
-/*   Updated: 2025/01/22 11:44:17 by seojilee         ###   ########.fr       */
+/*   Updated: 2025/01/23 10:23:32 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdlib.h>
 
-size_t	ft_strlen(const char *s)
+/*
+copy 조건:
+old_ptr && new_size
+*/
+static void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-	size_t	i;
+	unsigned char	*old_ptr;
+	unsigned char	*new_ptr;
+	size_t			alloc_size;
+	size_t			i;
 
-	i = 0;
-	while (*s)
+	old_ptr = (unsigned char *)ptr;
+	alloc_size = new_size;
+	if (!new_size && old_ptr)
+		alloc_size = 1;
+	new_ptr = malloc(alloc_size);
+	if (!new_ptr)
+		return (ptr);
+	if (new_size && old_ptr)
 	{
-		++s;
-		++i;
-	}
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	src_len;
-	size_t	cpsize;
-
-	src_len = ft_strlen(src);
-	if (dstsize != 0)
-	{
-		cpsize = dstsize - 1;
-		while (*src && cpsize > 0)
+		i = 0;
+		while (i < old_size && i < new_size)
 		{
-			*dst = *src;
-			++dst;
-			++src;
-			--cpsize;
+			new_ptr[i] = old_ptr[i];
+			++i;
 		}
-		*dst = '\0';
 	}
-	return (src_len);
+	free(ptr);
+	return (new_ptr);
 }
 
 char	*get_next_line(int fd)
